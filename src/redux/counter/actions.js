4dfcd377c9b050e27892efe0395035bbd38de9d6
Hayk Petrosyan <hpetrosyan.projects@gmail.com>
2021-09-09 12:06:42 +0200

    feat: created redirect function with state reseting

--- src/redux/counter/actions.js
+++ src/redux/counter/actions.js
@@ -1,27 +1,27 @@
-import $ from "jquery"
+import $ from "jquery";
 
 import {
   GET_PROPIERTIES,
   FAV_USER_PROPIERTIES,
   FILTER_PROPIERTIES,
-  CITY_PROPIERTIES
-} from './types'
+  CITY_PROPIERTIES,
+  RESET_PROPIERTIES,
+} from "./types";
 
 export const getPropierties = () => {
   return async (dispatch) => {
     try {
       const apiResult = await $.ajax({
-        url: "http://localhost:3000/properties", 
+        url: "http://localhost:3000/properties",
         type: "GET",
         success: (res) => {
-          //console.log(res);
-        } 
+          dispatch({ type: GET_PROPIERTIES, payload: res });
+        },
       });
-      dispatch({ type: GET_PROPIERTIES, payload: apiResult })
     } catch (error) {
       console.log("Error");
     }
-  }
+  };
 };
 
 // export const userFavPropierties = () => {
@@ -35,52 +35,54 @@ export const filterPropierties = () => {
   return async (dispatch) => {
     try {
       const apiResult = await $.ajax({
-        url: "http://localhost:3000/properties?price_gte=100000&price_lte=300000&room=5&bath=3&pet=true", 
+        url: "http://localhost:3000/properties?price_gte=100000&price_lte=300000&room=5&bath=3&pet=true",
         type: "GET",
         success: (res) => {
           console.log(res);
-        } 
+        },
       });
-      dispatch({ type: FILTER_PROPIERTIES, payload: apiResult })
+      dispatch({ type: FILTER_PROPIERTIES, payload: apiResult });
     } catch (error) {
       console.log("Error");
     }
-  }
+  };
 };
 
 export const getPropiertiesByCityName = (city) => {
   return async (dispatch) => {
     try {
       const apiResult = await $.ajax({
-        url: `http://localhost:3000/properties?city=${city}`, 
+        url: `http://localhost:3000/properties?city=${city}`,
         type: "GET",
         success: (res) => {
           console.log(res);
-        } 
+        },
       });
-      dispatch({ type: CITY_PROPIERTIES, payload: apiResult })
+      dispatch({ type: CITY_PROPIERTIES, payload: apiResult });
     } catch (error) {
       console.log("Error");
     }
-  }
+  };
 };
 
 export const getPropiertiesOffer = (offer) => {
   return async (dispatch) => {
     try {
       const apiResult = await $.ajax({
-        url: `http://localhost:3000/properties?offer=${offer}`, 
+        url: `http://localhost:3000/properties?offer=${offer}`,
         type: "GET",
         success: (res) => {
           console.log(res);
-        } 
+        },
       });
-      dispatch({ type: CITY_PROPIERTIES, payload: apiResult })
+      dispatch({ type: CITY_PROPIERTIES, payload: apiResult });
     } catch (error) {
       console.log("Error");
     }
-  }
+  };
 };
+
+export const resetPropierties = () => ({ type: RESET_PROPIERTIES });
 
 /*
 export const setRandomCounter = (maxValue) => {
