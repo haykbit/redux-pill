53c50dd0ca4701760c7e2c22e5318a7944dcc851
JonCroatan <43881219+jonCroatanUto@users.noreply.github.com>
2021-09-09 08:19:12 +0200

    form styles

--- src/components/FilterForm/FilterForm.jsx
+++ src/components/FilterForm/FilterForm.jsx
@@ -28,19 +28,14 @@ export default function FilterForm(){
         <form className="filters_form">
            
             <div className="typeHouse">
-
                 <div>
                     <input name="typeHouse" type="radio" id="HouseInput"></input>
                     <label for="HouseInput">House</label>
                 </div>
-
                 <div>
-                
                     <input name="typeHouse" type="radio" id="flat"></input>
-                    <label for="flat">Flat/apartament</label>
+                    <label for="flat">Flat/ apartament</label>
                 </div>
-
-
                 <div>
                     <input name="typeHouse" type="radio" id="penthouse"></input>
                     <label for="penthouse">Penthouse</label>
@@ -49,22 +44,6 @@ export default function FilterForm(){
                     <input name="typeHouse" type="radio" id="duplex"></input>
                     <label for="duplex">Duplex</label>
                 </div>
-            </div>
-
-            <div className="houseState">
-                <div>
-                    <input name="hose_State" type="radio" id="Needs_renovation"></input>
-                    <label for="Needs_renovation">Needs renovation</label>
-                </div>
-
-                <div>
-                    <input name="hose_State" type="radio" id="New_house"></input>
-                    <label for="New_house">New House</label>
-                </div>
-                <div>
-                    <input name="hose_State" type="radio" id="Good_condition"></input>
-                    <label for="Good_condition">Good condition</label> 
-                </div>  
             </div>
 
             <div className="bedrooms">
@@ -99,6 +78,51 @@ export default function FilterForm(){
                 </div>               
             </div>
 
+
+            <Select
+            onChange={onChangeEquipment}
+            >
+                <Option>Equipment 1</Option>
+                <Option>Equipment 2</Option>
+                <Option>Equipment 3</Option>
+                <Option>Equipment 4</Option>
+            </Select>
+
+            <div className="houseState">
+                <div>
+                    <input name="hose_State" type="radio" id="Needs_renovation"></input>
+                    <label for="Needs_renovation">Needs renovation</label>
+                </div>
+               <div>
+                    <input name="hose_State" type="radio" id="New_house"></input>
+                    <label for="New_house">New House</label>
+                </div>
+                <div>
+                    <input name="hose_State" type="radio" id="Good_condition"></input>
+                    <label for="Good_condition">Good condition</label> 
+                </div>  
+            </div>
+            
+            <RangeSlider
+               
+                endValue="250"
+                startValue="150"
+                showTooltip="true"
+                max="300.000"
+                min="100.000"
+                step="4.000"
+                onChange={onChangeRange}
+            />
+
+            <Select
+            onChange={onChangeHours}
+            >
+                <Option>last 48 hours</Option>
+                <Option>last 36 hours</Option>
+                <Option>last 24 hours</Option>
+                <Option>last 12 hours</Option>
+            </Select>
+
             <div className="moreFilters">
                 <div>
                     <input name="" type="checkbox" id="Needs_renovation"></input>
@@ -111,31 +135,6 @@ export default function FilterForm(){
                     <label for="Good_condition">Good condition</label>
                 </div>      
             </div>
-
-            <RangeSlider
-                endValue="5"
-                showTooltip="true"
-                max="300.000"
-                min="100.000"
-                step="4.000"
-                onChange={onChangeRange}
-            />
-            <Select
-            onChange={onChangeEquipment}
-            >
-                <Option>Equipment 1</Option>
-                <Option>Equipment 2</Option>
-                <Option>Equipment 3</Option>
-                <Option>Equipment 4</Option>
-            </Select>
-            <Select
-            onChange={onChangeHours}
-            >
-                <Option>last 48 hours</Option>
-                <Option>last 36 hours</Option>
-                <Option>last 24 hours</Option>
-                <Option>last 12 hours</Option>
-            </Select>
       </form>
     )
 }
