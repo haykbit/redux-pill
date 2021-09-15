67527ea9bfb47f2a32937e164ef30bee4da050fc
JonCroatan <43881219+jonCroatanUto@users.noreply.github.com>
2021-09-15 20:27:53 +0200

    aply bootstrap to login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,4 +1,4 @@
-
+import  "./styles.css";
 import * as React from 'react';
 import { Redirect } from 'react-router';
 import { login } from "../../redux/counter/actions";
@@ -34,17 +34,32 @@ export default function Login() {
       <>
       {sessionState==0 ?(
         <>
+       <div className="text-center" cz-shortcut-listen="true">
         <main class="form-signin">
-        <form>
+        <form onSubmit={sendData}>
           <img class="mb-4" src="/docs/5.1/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"/>
           <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
       
           <div class="form-floating">
-            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com"/>
+            <input 
+            type="email" 
+            class="form-control" 
+            id="floatingInput" 
+            onChange={handleSubmit}
+            defaultValue=""
+            name="email"
+            />
             <label for="floatingInput">Email address</label>
           </div>
           <div class="form-floating">
-            <input type="password" class="form-control" id="floatingPassword" placeholder="Password"/>
+            <input 
+            type="password" 
+            class="form-control" 
+            id="floatingPassword" 
+            onChange={handleSubmit}
+            defaultValue=""
+            name="password"
+            />
             <label for="floatingPassword">Password</label>
           </div>
       
@@ -57,7 +72,8 @@ export default function Login() {
           <p class="mt-5 mb-3 text-muted">© 2017–2021</p>
         </form>
       </main>
-             <form onSubmit={sendData}>
+      </div>
+             {/* <form onSubmit={sendData}>
              <label>
              <input 
              onChange={handleSubmit}
@@ -78,8 +94,8 @@ export default function Login() {
           
            <button type="submit">send</button>
          
-         </form>
-         </>
+         </form>*/}
+         </> 
           ):(
               <Redirect to={'./'} />
               
